ea4442d3aa6dd97fda7c8ae2bd8e88e06f1c987d
Manufer98 <manuelfernandezdelf98@gmail.com>
2022-02-03 16:59:42 -0300

    deploy update!!

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>My react app update</h1>
+      <h1>My react app update!!</h1>
       <h2>author</h2>
     </div>
   );
